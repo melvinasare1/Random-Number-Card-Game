7d12cfa0b57f8388ab34b1d35b41df9d1c03eacb
Melvin Asare <melvinasareb@gmail.com>
2020-05-13 19:06:29 +0100

    Initial Commit

---
+++ Swift UI Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Test
+//
+//  Created by melvin asare on 13/05/2020.
+//  Copyright © 2020 melvin asare. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
